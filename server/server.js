0e31b692b23a9379d3e74395ba9a3ddd3d930c15
Kevin <kevin.shehan30@gmail.com>
2023-07-26 19:15:30 +0530

    real time fetch data added

--- server/server.js
+++ server/server.js
@@ -3,18 +3,20 @@
 import  express  from 'express';
 import mongoose from "mongoose"
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const PORT = 4000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/',(req, res)=>{
   res.send("Hello World");
 });
 
 app.post('/transaction',(req, res)=>{
-  console.log(req.body);
+ const {amount,description, date}= req.body;
   res.json({message:"Hello World"});
 });
 
