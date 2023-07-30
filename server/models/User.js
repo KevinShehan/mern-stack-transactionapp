a825eef8ff8c996e63bf970149753c5bfced67d4
Kevin <kevin.shehan30@gmail.com>
2023-07-30 19:50:33 +0530

    user model updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    firstName: {type:String , required},
-    lastName:  {type:String , required},
-    email: {type:String , required},
-    password:  {type:String , required}, // String is shorthand for {type: String}
-    createdAt:{ type: Date, default: Date.now }
-});
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: ["FirstName field is Required"] },
+    lastName: { type: String, required: ["LastName field is Required"] },
+    email: { type: String, required: ["Email field is Required"] },
+    password: { type: String, required: ["Password field is Required"] }, // String is shorthand for {type: String}
+  },
+  { timestamps: true }
+);
 
-export default new mongoose.model("User",userSchema)
+export default new mongoose.model("User", userSchema);
