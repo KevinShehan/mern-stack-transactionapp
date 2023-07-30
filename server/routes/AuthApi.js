7be428e99e030f9d9971ea9e90a3b306b5e9eb1a
Kevin <kevin.shehan30@gmail.com>
2023-07-30 20:56:00 +0530

    bcrypt library install

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,16 +1,29 @@
 import { Router } from "express";
-
+import User from "../models/User.js";
 
 const router = Router();
-router.post('/register', (req, res) => {
-    // get all form data
-    //check if user exists with same email
-    // hash th password
-    // save user
-    const email= req.body;
-    console.log(req.body);
-    res.json({ message: "User is created" });
+router.post("/register", async (req, res) => {
+  // get all form data
+  // check if user exists with same email
+  // hash the password
+  // save user
+  const { email, password, firstName, lastName } = req.body;
+  // console.log(req.body);
+
+  const checkExists = await User.findOne({
+    email,
+    password,
+    firstName,
+    lastName,
   });
-  
+  if (checkExists) {
+    res.status(406).json({ message: "User Already Exists" });
+    return;
+  }
+  const user = await User(req.body);
+  const savedUser = user.save();
+  console.log(user);
+  res.json({ message: "User is created" });
+});
 
 export default router;
