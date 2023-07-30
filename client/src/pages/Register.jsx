a57e33180b2f6ff3236b59edcf074d2129f3ec55
Kevin <kevin.shehan30@gmail.com>
2023-07-30 15:52:18 +0530

    create Auth Api

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -22,6 +22,8 @@ export default function Register() {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
+            firstName: data.get('firstName'),
+            lastName: data.get('lastName'),
             email: data.get('email'),
             password: data.get('password'),
         });
